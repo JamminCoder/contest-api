da21ae95449a82a89aa7ffb84d260405fe7d4b07
JamminCoder <jammincoderguy96@gmail.com>
2022-08-03 10:47:56 -0400

    `port` set to `process.env.PORT`

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connectToDb = require('./db').default;
 
+
+dotenv.config();
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 
+// Connect to database
 connectToDb().catch(err => console.log(err));
 
 
-// Import controllers
-const AuthController = require('./controllers/AuthController').default;
-
-
+// Register middleware
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.post('/register', AuthController.register);
+// Import controllers
+const AuthController = require('./controllers/AuthController').default;
 
+
+
+// Routes
+app.post('/register', AuthController.register);
 app.post('/login', AuthController.login);
+
+
 
 
 app.listen(port, () => {
